4148c3e2efa61e100e3ea8d111e84ec11240d60b
kensiiwasaki <kensiiwasaki@gmail.com>
2024-03-25 00:34:52 +0900

    shallow: false で変更ごとにリクエストしてくれる

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-// import { useQueryState } from 'nuqs';
+// import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useQueryState } from 'nuqs';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  // const [query, setQuery] = useQueryState('query');
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+  const [query, setQuery] = useQueryState('query', { shallow: false });
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
+  // function handleSearch(term: string) {
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
 
-    replace(`${pathname}?${params.toString()}`);
-  }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -29,12 +29,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        // onChange={(e) => setQuery(e.target.value)}
-        // value={query || ''}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => setQuery(e.target.value)}
+        value={query || ''}
+        // onChange={(e) => {
+        //   handleSearch(e.target.value);
+        // }}
+        // defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
