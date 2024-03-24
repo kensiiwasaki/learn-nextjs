b5dfdc2d8b1dca1a40f690d3dd78b67d07935c75
kensiiwasaki <kensiiwasaki@gmail.com>
2024-03-25 00:24:17 +0900

    やっぱコメント残す

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,25 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useQueryState } from 'nuqs';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const [query, setQuery] = useQueryState('query');
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
+
+  // function handleSearch(term: string) {
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
